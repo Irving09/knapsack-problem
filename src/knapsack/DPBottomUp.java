14094cb0d78264cc59cd8efc4e6ab79ad3cd1e00
Inno Irving V. Estrera <innoirving.estrera@nordstrom.com>
2018-11-18 12:02:09 -0800

    added bottom up implementation

--- src/knapsack/DPBottomUp.java
+++ src/knapsack/DPBottomUp.java
@@ -9,12 +9,29 @@
  */
 package knapsack;
 
+import static java.lang.Math.max;
+
 /**
  * @author irving09 <innoirvinge@gmail.com>
  */
 public class DPBottomUp implements Knapsack {
     @Override
     public int solve(int[] weights, int[] values, int capacity) {
+        int n = weights.length;
+
+        int[][] dp = new int[n + 1][capacity + 1];
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < capacity; j++) {
+                if (weights[i] > j) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    int included = dp[i - 1][ j - weights[i]] + values[i];
+                    int notIncluded = dp[i - 1][j];
+
+                    dp[i][j] = max(notIncluded, included);
+                }
+            }
+        }
         return 0;
     }
 }
