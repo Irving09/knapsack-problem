c3d74d88d2c112ae959f92fba5d6815ff8bef566
Inno Irving V. Estrera <innoirving.estrera@nordstrom.com>
2018-11-30 13:56:29 -0800

    latest settings on mine

--- src/tests/performance/TestSetGenerator.java
+++ src/tests/performance/TestSetGenerator.java
@@ -19,15 +19,15 @@ import static java.util.concurrent.ThreadLocalRandom.current;
  */
 public class TestSetGenerator {
 
+    private static final int MIN_ITEMS = 1;
     private static final int MAX_ITEMS_FOR_BRUTE_FORCE = 20;
     private static final int VALUE_LIMIT = 1000;
-    private static final int WEIGHT_LIMIT = 1024;
-    private static final int NUM_TEST_CASES_PER_BATCH = 50;
+    private static final int WEIGHT_LIMIT = 1000;
 
     public static List<TestSet> testsWithConstantCapacity() {
         List<TestSet> testSets = new ArrayList<>();
         for (int capacity = 10; capacity <= WEIGHT_LIMIT; capacity += 50) {
-            testSets.add(generateTestSetConstantCapacity(NUM_TEST_CASES_PER_BATCH, capacity));
+            testSets.add(generateTestSetConstantCapacity(capacity));
         }
 
         return testSets;
@@ -35,29 +35,29 @@ public class TestSetGenerator {
 
     public static List<TestSet> testsWithConstantItems() {
         List<TestSet> testSets = new ArrayList<>();
-        for (int itemSize = 10; itemSize <= MAX_ITEMS_FOR_BRUTE_FORCE; itemSize++) {
-            testSets.add(generateTestSetConstantItemSize(NUM_TEST_CASES_PER_BATCH, itemSize));
+        for (int itemSize = MIN_ITEMS; itemSize <= MAX_ITEMS_FOR_BRUTE_FORCE; itemSize++) {
+            testSets.add(generateTestSetConstantItemSize(itemSize));
         }
 
         return testSets;
     }
 
-    public static TestSet generateTestSetConstantCapacity(int numTestCases, int capacity) {
+    public static TestSet generateTestSetConstantCapacity(int capacity) {
 
-        List<TestCase> testCases = new ArrayList<>(numTestCases);
+        List<TestCase> testCases = new ArrayList<>(MAX_ITEMS_FOR_BRUTE_FORCE);
 
-        for (int itemSize = 10; itemSize <= MAX_ITEMS_FOR_BRUTE_FORCE; itemSize++) {
+        for (int itemSize = MIN_ITEMS; itemSize <= MAX_ITEMS_FOR_BRUTE_FORCE; itemSize++) {
             testCases.add(generateTestCase(itemSize, capacity));
         }
 
         return new TestSet(testCases, 0, capacity);
     }
 
-    public static TestSet generateTestSetConstantItemSize(int numTestCases, int itemSize) {
+    public static TestSet generateTestSetConstantItemSize(int itemSize) {
 
-        List<TestCase> testCases = new ArrayList<>(numTestCases);
+        List<TestCase> testCases = new ArrayList<>(WEIGHT_LIMIT);
 
-        for (int capacity = 2; capacity <= WEIGHT_LIMIT; capacity *= 2) {
+        for (int capacity = 2; capacity <= WEIGHT_LIMIT; capacity += 2) {
             testCases.add(generateTestCase(itemSize, capacity));
         }
 
@@ -69,14 +69,14 @@ public class TestSetGenerator {
         int[] weights = new int[itemSize];
         for(int j = 0; j < itemSize; j++) {
             values[j] = current().nextInt(VALUE_LIMIT);
-            weights[j] = current().nextInt(WEIGHT_LIMIT);
+            weights[j] = current().nextInt(capacity);
         }
 
         return TestCase.builder()
-            .capacity(capacity)
-            .weights(weights)
-            .values(values)
-        .build();
+                .capacity(capacity)
+                .weights(weights)
+                .values(values)
+                .build();
     }
 
 }
